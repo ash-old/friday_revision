81442dcd96e4cb3efeeb8f9211664cfcfdc12977
ash-old <ashley.oldham@gmail.com>
2020-05-08 11:48:41 +0100

    initial commit for revision session

---
+++ revision.rb
@@ -0,0 +1,12 @@
+
+#this method should loop over all friends to see if a given friends name
+#exits in the array of friends
+#if yes, return true
+#if not, return false
+def is_friend?(all_friends, friend_name)
+  #loop over each friend in friend array
+    # get friends's name
+    #if friends's name == friend_name
+    #return true
+  #at end of loop, if friend name not found return false
+end
